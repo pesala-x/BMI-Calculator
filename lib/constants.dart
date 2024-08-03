654f360b200d62ffe267f2f7214fa54820d63d3b
pesala-x <gkpesalawinodith@gmail.com>
2024-08-03 23:42:29 +0530

    constants and main ui created

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(0xFFFF8888);
+const kOutputTextColor = Color(0xFFFF5555);
+
+const kInputLabelColor = TextStyle(
+  color: kTextColor,
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+);
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -11,27 +11,127 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:     SafeArea(
+      body: SafeArea(
         child: Container(
-        color: Colors.white,
-        child: Column(children: [
-          Row(children: [
-            Column(
+          color: Colors.white,
+          child: const Column(children: [
+            Row(children: [
+              //First Raw Icons with name
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Icon(Icons.male, size: 150),
+                    Text("male"),
+                  ],
+                ),
+              ),
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Icon(Icons.female, size: 150),
+                    Text("female"),
+                  ],
+                ),
+              ),
+            ]),
+            Row(
+              //Second Raw Buttons
               children: [
-                Text("Male Icon"),
-                Text("male"),
+                Padding( //Body Height section 
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text("Height"),
+                      Text("176",
+                          style: TextStyle(
+                              color: Color(0xFFFF8888),
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold)),
+                      Row(
+                        children: [
+                          FloatingActionButton(
+                            shape: CircleBorder(),
+                            backgroundColor: Color.fromARGB(255, 29, 153, 255),
+                            onPressed: null,
+                            child: Icon(
+                              Icons.remove,
+                              size: 30,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 25,
+                          ),
+                          FloatingActionButton(
+                            shape: CircleBorder(),
+                            backgroundColor: Color.fromARGB(255, 29, 153, 255),
+                            onPressed: null,
+                            child: Icon(
+                              Icons.add,
+                              size: 30,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Padding(//Body Height section
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text("Weight"),
+                      Text("76",
+                          style: TextStyle(
+                              color: Color(0xFFFF8888),
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold)),
+                      Row(
+                        children: [
+                          FloatingActionButton(
+                            shape: CircleBorder(),
+                            backgroundColor: Color.fromARGB(255, 29, 153, 255),
+                            onPressed: null,
+                            child: Icon(
+                              Icons.add,
+                              size: 30,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 25,
+                          ),
+                          FloatingActionButton(
+                            shape: CircleBorder(),
+                            backgroundColor: Color.fromARGB(255, 29, 153, 255),
+                            onPressed: null,
+                            child: Icon(
+                              Icons.remove,
+                              size: 30,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
             Column(
               children: [
-                Text("Female Icon"),
-                Text("female"),
+                Text("BMI"),Text("25.3"),
               ],
-            ),
-          ])
-        ]),
-            ),
+            ), 
+          ]
+          ),
+        ),
       ),
-    ); 
+    );
   }
 }
